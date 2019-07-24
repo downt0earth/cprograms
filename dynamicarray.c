8ab615fc0794faf3442bf0ec5f0f2be1117d7068
pihugit <53227376+pihugit@users.noreply.github.com>
2019-07-24 03:07:23 -0700

    Create dynamicarray.c

---
+++ dynamicarray.c
@@ -0,0 +1,25 @@
+Java Group 1 July-2019
+Class Notes 23.07.2019
+
+Dr.Bhuvneshwar Prasad Sharma-MBA
+
+/*
+	WAP to ask the user how many numbers to input, input that much of numbers and show 
+	sum of all given numbers.
+*/
+main()
+{
+	int n,*p,i,sum=0;
+	printf("How many numbers to input : ");
+	scanf("%d",&n);
+	p=malloc(n*sizeof(int));
+	printf("Enter %d numbers : ",n);
+	for(i=0;i<n;i++)
+	 scanf("%d",&p[i]);
+	
+	for(i=0;i<n;i++)
+	  sum=sum+p[i];
+	printf("Sum is %d",sum);
+}
+	
+	
